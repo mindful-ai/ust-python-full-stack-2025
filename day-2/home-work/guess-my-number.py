a60690650bf92f66d809749e5a65e123b8218979
mindful-ai <mindfullearningindia@outlook.com>
2025-10-09 17:43:19 +0530

    Add files via upload

--- day-2/home-work/guess-my-number.py
+++ day-2/home-work/guess-my-number.py
@@ -3,3 +3,21 @@
 # After each guess, the program should tell the user 
 # if the guess is too high, too low, or correct. If the user guesses the number correctly, 
 # they win; otherwise, they lose after 10 attempts.
+
+
+'''
+The computer will think of a number between 1 and 100.
+You have 10 attempts to guess the number.
+
+Guess -> 50
+Guess low
+
+Guess -> 40
+Guess high
+
+Guess -> 45
+Correct! You win!
+
+Points: 8
+
+'''
