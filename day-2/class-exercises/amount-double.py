ee26e6e371617665b2c707d90a53ca340414692d
mindful-ai <mindfullearningindia@outlook.com>
2025-10-09 13:33:12 +0530

    Add files via upload

---
+++ day-2/class-exercises/amount-double.py
@@ -0,0 +1,2 @@
+# A person wants to double an amount every for a given rate of interest
+# Calculate how much time is required to double the amount
---
+++ day-2/home-work/guess-my-number.py
@@ -0,0 +1,5 @@
+# Create the "guess my number" game where the user has to guess a number between 1 and 100. 
+# The user has 10 attempts to guess the number. 
+# After each guess, the program should tell the user 
+# if the guess is too high, too low, or correct. If the user guesses the number correctly, 
+# they win; otherwise, they lose after 10 attempts.
