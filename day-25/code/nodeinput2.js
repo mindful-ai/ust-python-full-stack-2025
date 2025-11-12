b71922e8d5b4ae827abe2e74c17ec6108a1492fe
mindful-ai <mindfullearningindia@outlook.com>
2025-11-12 10:34:24 +0530

    Add files via upload

---
+++ day-25/code/nodeinput2.js
@@ -0,0 +1,21 @@
+// Make sure to install the module prompt using npm
+
+const prompt = require('prompt');
+
+prompt.start();
+
+var name = "";
+var email = "";
+
+prompt.get(['username', 'email'], function (err, result) {
+    if (err) { return onErr(err); }
+    name  = result.username 
+    email = result.email
+    console.log(name.length + 78)
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
+
